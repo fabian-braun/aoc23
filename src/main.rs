b664d40f23a1e39d83823c4ed2eb0ce085aea887
Fabian Braun <fabian-braun@users.noreply.github.com>
2023-12-01 09:19:32 +0100

    Implement first solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::read_to_string;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let numbers = &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let calibration_value_sum: usize = read_to_string("input_day1")
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .map(|s| {
+            let i_first = s.find(numbers).unwrap();
+            let i_last = s.rfind(numbers).unwrap();
+            let first = usize::from_str(&s.chars().nth(i_first).unwrap().to_string()).unwrap();
+            let last = usize::from_str(&s.chars().nth(i_last).unwrap().to_string()).unwrap();
+            println!("{first}{last}");
+            first + last
+        })
+        .sum();
+    println!("Hello, world! The calibration sum is {}", calibration_value_sum);
 }
