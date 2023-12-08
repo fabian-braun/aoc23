e063cf4f28f19e49ab69c51b88930aba8a19bbd2
Fabian Braun <fabian-braun@users.noreply.github.com>
2023-12-08 22:00:26 +0100

    Solve day 8 part 1

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,16 +1,40 @@
-use std::collections::HashSet;
-use std::fmt::Display;
+use std::collections::HashMap;
 use std::fs::read_to_string;
-use std::str::FromStr;
 use itertools::Itertools;
 
 fn main() {
-    let file_context = read_to_string("example_day8").unwrap();
-    let result: usize = file_context.lines()
-        .map(|line| {
-        }).count();
+    let file_context = read_to_string("input_day8").unwrap();
+    let policy = file_context.lines().next().unwrap().chars().map(|c| {
+        match c {
+            'L' => { true }
+            _ => { false }
+        }
+    }).collect_vec();
 
-    println!("Part I solution: {}", result);
+    let mut map = HashMap::<&str, (&str, &str)>::new();
+    file_context.lines()
+        .skip(2)
+        .for_each(|line| {
+            let (src, left_right) = line.split_once(" = (").unwrap();
+            let (left, right) = left_right.strip_suffix(')').unwrap().split_once(", ").unwrap();
+            map.insert(src, (left, right));
+        });
+    let mut current = "AAA";
+    let mut policy_index = 0usize;
+    let mut steps = 0usize;
+    while current != "ZZZ" {
+        let (left, right) = &map[&current];
+        current = if policy[policy_index] {
+            left
+        } else {
+            right
+        };
+        policy_index += 1;
+        policy_index %= policy.len();
+        steps += 1;
+    }
+
+    println!("Part I solution: {}", steps);
 }
 
 #[cfg(test)]
