a00bb3714f6dee80979a5ca58bc4dd4c4469c05f
Fabian Braun <fabian-braun@users.noreply.github.com>
2023-12-08 21:38:17 +0100

    continue day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -14,8 +14,8 @@ fn main() {
         .map(|chunk| {
             let start = chunk[0];
             let len = chunk[1];
-            start..start + len
-        }).flatten();
+            (start, start + len)
+        }).collect_vec();
     let mapping: HashMap<(String, String), Mapping> = lines.iter().dropping(1).map(|l| {
         str_to_mapping(l)
     }).map(|m| {
@@ -32,13 +32,13 @@ fn main() {
     let temperature_to_humidity = &mapping[&("temperature".to_string(), "humidity".to_string())];
     let humidity_to_location = &mapping[&("humidity".to_string(), "location".to_string())];
 
-    let min_loc = seeds.map(|seed| {
-        let tmp =      seed_to_soil.to_dst(seed);
-        let tmp =      soil_to_fertilizer.to_dst(tmp);
-        let tmp =      fertilizer_to_water.to_dst(tmp);
-        let tmp =      water_to_light.to_dst(tmp);
-        let tmp =      light_to_temperature.to_dst(tmp);
-        let tmp =      temperature_to_humidity.to_dst(tmp);
+    let min_loc = seeds.into_iter().map(|(seed_range_start_incl, seed_range_end_incl)| {
+        let tmp = seed_to_soil.to_dst_min((seed_range_start_incl, seed_range_end_incl));
+        let tmp = soil_to_fertilizer.to_dst(tmp);
+        let tmp = fertilizer_to_water.to_dst(tmp);
+        let tmp = water_to_light.to_dst(tmp);
+        let tmp = light_to_temperature.to_dst(tmp);
+        let tmp = temperature_to_humidity.to_dst(tmp);
         let location = humidity_to_location.to_dst(tmp);
         location
     }).min().unwrap();
@@ -67,6 +67,47 @@ impl Mapping {
             Err(_) => { src }
         }
     }
+
+    pub fn to_dst_with_bounds(&self, src: i64) -> (i64, (i64, i64)) {
+        let idx = self.mappings.binary_search_by(|mapping| {
+            if src >= mapping.start_incl && src < mapping.end_excl {
+                Ordering::Equal
+            } else if src < mapping.start_incl {
+                Ordering::Greater
+            } else { Ordering::Less }
+        });
+        match idx {
+            Ok(idx) => { (src + self.mappings[idx].offset, (self.mappings[idx].start_incl, self.mappings[idx].end_excl)) }
+            Err(idx) => {
+                // try find neighboring mappings
+                let start_incl = if idx > 0 {
+                    self.mappings[idx - 1].end_excl
+                } else {
+                    // src is smaller than smallest start of all mappings
+                    src
+                };
+                let end_incl = if idx < self.mappings.len() {
+                    self.mappings[idx].start_incl - 1
+                } else {
+                    src
+                };
+                (src, (start_incl, end_incl))
+            }
+        }
+    }
+
+    pub fn to_dst_min(&self, (mut src_start_incl, src_end_excl): (i64, i64)) -> i64 {
+        let mut min = self.to_dst(src_start_incl);
+        while src_start_incl < src_end_excl {
+            let (start_min, mapping_bounds) = self.to_dst_with_bounds(src_start_incl);
+            let (_, mapping_end_excl) = mapping_bounds;
+            let test_upper_end = src_end_excl.min(mapping_end_excl);
+            min = min.min(start_min.min(self.to_dst(test_upper_end - 1)));
+            src_start_incl = test_upper_end;
+        }
+        min
+    }
+
     pub fn to_src(&self, dst: i64) -> i64 {
         let idx = self.mappings.binary_search_by(|mapping| {
             if dst >= mapping.start_incl + mapping.offset && dst < mapping.end_excl + mapping.offset {
