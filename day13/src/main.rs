378e3dab277c4f25ddc595669408a6b55244fec5
Fabian Braun <fabian-braun@users.noreply.github.com>
2023-12-13 22:35:45 +0100

    Change matrix type to bool

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -6,12 +6,12 @@ async fn main() {
     let result: usize = content.split("\n\n").map(|pattern| {
         let y_len: usize = pattern.lines().count();
         let x_len: usize = pattern.lines().next().unwrap().len();
-        let mut map = Array2::from_elem((y_len, x_len), '.');
+        let mut map = Array2::from_elem((y_len, x_len), false);
         pattern.lines()
             .enumerate().for_each(|(y, line)| {
             line.chars().enumerate()
                 .for_each(|(x, c)| {
-                    map[(y, x)] = c;
+                    map[(y, x)] = c == '#';
                 });
         });
         let mut score = find_reflection_rows(map.clone()) * 100;
@@ -25,8 +25,7 @@ async fn main() {
     println!("Part I solution: {}", result);
 }
 
-fn find_reflection_rows(pattern: Array2<char>) -> usize {
-    println!("{}", pattern);
+fn find_reflection_rows(pattern: Array2<bool>) -> usize {
     let y_min: usize = 0;
     let y_max: usize = pattern.len_of(Axis(0)) - 1;
     'outer: for reflection_index in y_min..y_max {
@@ -34,9 +33,7 @@ fn find_reflection_rows(pattern: Array2<char>) -> usize {
         let orig_y_start = reflection_index + 1 - elems_per_reflection;
         let mut y_start = orig_y_start;
         let mut y_end = y_start + elems_per_reflection + elems_per_reflection - 1;
-        println!("elems per reflection {} in {},{}", elems_per_reflection, y_start, y_end);
         while y_start < y_end {
-            println!("{} == {}", pattern.row(y_start), pattern.row(y_end));
             if pattern.row(y_start) != pattern.row(y_end) {
                 continue 'outer;
             }
@@ -44,7 +41,6 @@ fn find_reflection_rows(pattern: Array2<char>) -> usize {
             y_end -= 1;
         }
         let rows_above = elems_per_reflection + orig_y_start;
-        println!("Found reflection {}", rows_above);
         return rows_above;
     }
     0
